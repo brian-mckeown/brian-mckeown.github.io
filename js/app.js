af3f93b4b84f1f5f7a90d3ab2a05ec6593482319
Brian McKeown <bmckeown2011@gmail.com>
2022-03-06 11:53:56 -0500

    v1 commit

--- js/app.js
+++ js/app.js
@@ -6,13 +6,31 @@ app.controller("mainController", function($scope, $http) {
   const linkedInUser = 'bmckeown2011';
   $scope.githubProfileUrl = 'https://www.github.com/' + githubUser;
   $scope.linkedInUrl = 'https://www.linkedin.com/in/' + linkedInUser;
+  $scope.stackOverflowUrl = 'https://stackoverflow.com/users/5834929/' + githubUser;
   $scope.name = "Brian McKeown";
+  $scope.email = "bmckeown2011@gmail.com";
   $scope.slogan = "Programmer - Problem Solver - Learner";
-  $scope.projectInfo = "My hobby and some professional projects are all hosted on Github. The content below is displayed on this page via Github API integration. Click the links for each project to learn more!";
-  $scope.aboutInfo1 = "My name is Brian and I am a programmer, problem solver, and life-long learner. The purpose of this site is to present some of the projects I have worked on in the past, or am currently working on. You can learn more about my experience and background by clicking the LinkedIn button below!";
-  $scope.aboutInfo2 = "Outside of work and study, you'll find me hanging out with my wife and our two dogs. I enjoy music, outdoor adventures, mountain biking, snowboarding, and of course, coding.";
+  $scope.projectInfo = "My hobby and some professional projects are all hosted on Github. Only projects that have been configured as public are shown. I have many other private projects as well. The content below is displayed on this page via Github API integration. Click the links for each project to learn more!";
+  $scope.aboutInfo1 = "Hi, I'm Brian! I'm a professional, full stack Software Engineer with experience in developing information systems. The purpose of this site is to present some of the projects I have worked on in the past, or am currently working on. You can learn more about my experience and background by clicking the LinkedIn button below!";
+  $scope.aboutInfo2 = "Outside of working on software, you'll find me hanging out with my wife and our two dogs. I enjoy music, flying drones, outdoor adventures, mountain biking, snowboarding, and of course, coding.";
 
+  
   $scope.cards = [];
+
+    $scope.frontendSkills = ['AngularJS','HTML5','CSS3','Javascript','JSX','React','EJS','ES6','Bootstrap'].sort();
+    $scope.backendSkills = ['NodeJS','Spring-Boot','ExpressJS','Java','C-Sharp','Python'].sort();
+    $scope.dbSkills = ['PostgreSQL','MySQL','mongoDB','SQL','Hibernate'].sort();
+    $scope.integrationSkills = ['JSON','REST API'].sort();
+    $scope.devtoolSkills = ['npm','Gradle','Maven','Git','Github','Bitbucket','Trello','Jira','Android Studio','Visual Studio Code','Eclipse','IntelliJ IDEA','Postman'].sort();
+    $scope.sysadminSkills = ['Tomcat','PM2','okta','Bash'].sort();
+    $scope.lowcodeSkills = ['Zoho','Pega'].sort();
+    $scope.osSkills = ['Android','Raspbian','MacOS','Windows','Ubuntu','CentOS','Linux Mint','Linux', 'Debian'].sort();
+
+
+    $scope.getYear = function() {
+      const d = new Date();
+      return d.getFullYear();
+    }
 
   $scope.getPropertyName = function (language) {
     console.log("test function: " + language);
@@ -22,33 +40,24 @@ app.controller("mainController", function($scope, $http) {
     method: 'GET',
     url: $scope.cards[i].languages_url
   }).then(function (response) {
-    console.log("Entered then - loop #:" + i);
-    console.log("Object.keys(response.data).length = " + Object.keys(response.data).length);
     if (Object.keys(response.data).length > 0) {
-      console.log("Object.keys(response.data).length > 0 is TRUE");
       $scope.cards[i].languages_obj = response.data;
-      console.log("response.data = " + response.data);
-      console.log("attempting to set.");
-      console.log("$scope.cards["+i+"].languages_obj = " + $scope.cards[i].languages_obj);
       var languageArray = [];
-      console.log("language Array initiated as blank: " + languageArray);
       for (const property in $scope.cards[i].languages_obj) {
         languageArray.push(property);
-        console.log("language Array push. i="+i+" array: " + languageArray);
         $scope.cards[i].languages_array = languageArray;
-        console.log("Language Array " + i + ": " + $scope.cards[i].languages_array);
       }
   } else {
-    console.log("Object.keys(response.data).length > 0 is FALSE");
     $scope.cards[i].languages_array = [];
-    console.log("Language Array " + i + ": " + $scope.cards[i].languages_array);
   }
   },function (error){
     console.log('Error: ' + error);
+    alert('Github Public API Limit Reached. Some or All Projects may not be displayed. Use the Github button to view projects. Or, try again in an hour.\nError:' + error);
    });
   }
 
   //get all github repos.
+  
   $http({
     method: 'GET',
     url: githubURL + '/users/' + githubUser + '/repos'
@@ -63,6 +72,7 @@ app.controller("mainController", function($scope, $http) {
 
  },function (error){
   console.log('Error: ' + error);
+  alert('Github Public API Limit Reached. Some or All Projects may not be displayed. Use the Github button to view projects. Or, try again in an hour.\nError:' + error);
  });
 
 
